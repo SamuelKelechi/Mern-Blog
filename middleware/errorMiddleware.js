0b1d108a14d3999f017aa54381dc97ca24692093
SamuelKelechi <kcfreshie@ymail.com>
2025-01-08 16:25:17 +0100

    Handling Error File

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,30 +1,18 @@
 //Unsupported (404) routes
-
-    const notFound = (req, res, next) => {
-        const error = new Error(`Not Found - ${req.originalUrl}`);
-        error.status = 404;
-        next(error);
-      };
+const notFound = (req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+}
 
 // Middleware to handle Errors
-const errorHandler = (err, req, res, next) => {
-    console.error(err);
-
-    // Handle Multer-specific errors
-    if (err.code === 'LIMIT_UNEXPECTED_FILE') {
-        return res.status(400).json({
-            message: "Unexpected file field. Please ensure the file key is 'image'."
-        });
+const errorHandler = (error, req, res, next) => {
+    if(res.headerSent) {
+        return next(error);
     }
 
-    if (res.headersSent) {
-        return next(err);
-    }
-
-    res.status(err.status || 500).json({
-        message: err.message || "An unexpected error occurred.",
-    });
-};
+    res.status(error.code || 500).json({message: error.message || "An unknown error occurred"})
+}
 
 
 module.exports = {notFound, errorHandler}
